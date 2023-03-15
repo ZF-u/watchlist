0aa48486d7830362be700091da5d7b0eeff6fda2
zfu <f386106606@gmail.com>
2023-03-15 14:36:37 +0800

    User authentication with Flask-Login

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ from flask import Flask, render_template, session
 from markupsafe import escape
 from flask import request, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_login import UserMixin
+from flask_login import login_user #login func
+from flask_login import login_required, logout_user, current_user #logout func & current_user func
+#导入生成和验证密码散列值的函数
+from werkzeug.security import generate_password_hash, check_password_hash
 
 WIN = sys.platform.startswith('win') #兼容处理
 if WIN:
@@ -20,6 +26,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'da
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'dev'
 db = SQLAlchemy(app) #初始化扩展，传入程序实例 app
+login_manager = LoginManager(app) #init extend class
+login_manager.login_view = 'login' 
+
+@login_manager.user_loader
+def load_user(user_id): # 创建用户加载回调函数，接受用户 ID 作为参数
+    user = User.query.get(int(user_id)) # 用 ID 作为 User 模型的主键查询对应的用户
+    return user  # 返回用户对象
 
 #自定义命令initdb
 @app.cli.command()
@@ -58,6 +71,26 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username used to login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
+def admin(username, password):
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updationg user...')
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)
+        db.session.add(user)
+
+    db.session.commit()
+    click.echo('Done.')
+
 @app.context_processor
 def inject_user():
     user = User.query.first()
@@ -76,19 +109,78 @@ def unable_handel(e):
     return render_template('505.html'), 505
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True) #primary_k 设置主键
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20)) #username
+    password_hash = db.Column(db.String(128)) #pw散列值
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+        #Verify
+        if username == user.username and user.validate_password(password):
+            login_user(user)
+            flash('Login success.')
+            return redirect(url_for('index'))
+
+        flash('Invalid username or password.')
+        return redirect(url_for('index'))
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required #视图保护 指的是页面上有些内容需要对未登录用户隐藏
+#进一步，未登录的用户不能执行：访问编辑页面、访问设置页面、执行注销操作、执行删除操作、执行添加新条目操作
+def logout():
+    logout_user()
+    flash('Bye.')
+    return redirect(url_for('index'))
+
+
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        if not name or len(name) > 20:
+            flash('Invalid input.')
+            return redirect(url_for('settings'))
+
+        current_user.name = name
+        db.session.commit()
+        flash('Settings updated.')
+        return redirect(url_for('index'))
+
+    return render_template('settings.html')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST' :
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
+
         title = request.form.get('title')
         year = request.form.get('year')
 
@@ -106,6 +198,7 @@ def index():
     return render_template('index.html', movies=movies)
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
 
@@ -126,6 +219,7 @@ def edit(movie_id):
     return render_template('edit.html', movie=movie)
 
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
